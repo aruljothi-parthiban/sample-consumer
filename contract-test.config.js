5ceaed334beb126990e7fcfdf453d050925a923d
Aruljothi Parthiban <aruljothiparthiban@hotmail.com>
2022-11-07 13:07:54 +0530

    swagger hub integration

--- contract-test.config.js
+++ contract-test.config.js
@@ -3,12 +3,16 @@ module.exports = {
   providers: [
     {
       name: "sample-provider",
-      version: "1.0.0",
-      swaggerFilePath: "../provider/oas/products.yml",
+      version: "1.0.1",
+      // swaggerFilePath: "../provider/oas/products.yml",
       paths: {
         "/products": "./responses/get_responses.json",
         // "/{baseSiteId}/products/{productCode}"
       },
     },
   ],
+  swaggerHub: {
+    owner: "CantireCorp",
+    API_KEY: "7a8e2166-0b76-4f6a-aff0-76486b1aa3fe",
+  },
 };
